af6b2ebf90ad4ac9c0d9b9960fe9c84e85048dba
Benjamin Doherty <bjd@bangpound.org>
2016-11-29 07:28:10 -0600

    remote php shunt scripts.

--- php/src/app.php
+++ php/src/app.php
@@ -34,7 +34,7 @@ $app['shunt'] = function (\Silex\Application $app) {
 
 
     $recipe = array(
-      'hosts' => $hosts['ec2'],
+      'hosts' => $hosts['bastion'],
       'auth' => array(
         'username' => 'debian',
         'pubkeyfile' => '../ucsb-eucalyptus/bama.pub',
@@ -43,19 +43,25 @@ $app['shunt'] = function (\Silex\Application $app) {
       ),
 
       'tasks' => array(
-        'vmstat' => function ($s) {
+        'worker-streaming' => function (\League\Shunt\Shunt $s) {
+            $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 500-worker-streaming.yml');
+        },
+          'web-update' => function (\League\Shunt\Shunt $s) {
+              $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 500-web-update.yml');
+          },
+        'vmstat' => function (\League\Shunt\Shunt $s) {
             $s->run('vmstat -w');
             $s->run('uptime');
             $s->run('df');
             $s->run('free -h');
         },
-        'read_home_dir' => function($s) {
+        'read_home_dir' => function(\League\Shunt\Shunt $s) {
             $s->run('ls');
         },
-        'print_php_info' => function($s) {
+        'print_php_info' => function(\League\Shunt\Shunt $s) {
             $s->run('php -i');
         },
-        'upload_foo_source' => function($s) {
+        'upload_foo_source' => function(\League\Shunt\Shunt $s) {
             $s->sftp()->mkdir('source');
             $s->scp()->put('foo', 'source/foo');
         }
