37363e74295f1b7acba69348b2c494be35c68380
Benjamin Doherty <bjd@bangpound.org>
2016-12-31 16:58:25 -0600

    make a new inventory script.

---
+++ php/src/InventoryCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+use Aws\AwsClientInterface;
+use Aws\Ec2\Ec2Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+
+/**
+ * Class InventoryCommand
+ */
+class InventoryCommand extends Command
+{
+    const SEARCH_EXPRESSION = 'Reservations[].Instances[?State.Name==\'running\'][]';
+
+    /**
+     * @var AwsClientInterface|Ec2Client
+     */
+    private $client;
+
+    /**
+     * @var CamelCaseToSnakeCaseNameConverter
+     */
+    private $converter;
+
+    /**
+     * InventoryCommand constructor.
+     * @param AwsClientInterface $client
+     */
+    public function __construct(AwsClientInterface $client)
+    {
+        $this->client = $client;
+        $this->converter = new CamelCaseToSnakeCaseNameConverter();
+        parent::__construct('ec2:inventory');
+    }
+
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this->setName('ec2:inventory');
+        $this->addOption('list');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var \Aws\Result $result */
+        $result = $this->client->describeInstances();
+        $instances = (array) $result->search(self::SEARCH_EXPRESSION);
+
+        $inventory = ['_meta' => ['hostvars' => []]];
+
+        foreach ($instances as $instance) {
+            $instanceVars = $instance;
+            $hostname = $this->isBastion($instance) ? $instance['PublicDnsName'] : $instance['PrivateDnsName'];
+            $inventory['_meta']['hostvars'][$hostname] = $instanceVars;
+            $inventory['ec2'][] = $hostname;
+
+            foreach ($instance['Tags'] as $tag) {
+                $combinedKey = $tag['Key'] .'_'. $tag['Value'];
+                $inventory[$tag['Key']]['children'][] = $combinedKey;
+                $inventory[$combinedKey][] = $hostname;
+            }
+        }
+
+
+        $output->writeln(json_encode($inventory));
+    }
+
+    /**
+     * @param $instance
+     * @return bool
+     */
+    private function isBastion($instance)
+    {
+        return !empty(array_filter($instance['Tags'], function ($tag) {
+            return $tag['Key'] === 'bastion' && filter_var($tag['Value'], FILTER_VALIDATE_BOOLEAN) === true;
+        }));
+    }
+}
--- php/src/app.php
+++ php/src/app.php
@@ -1,80 +1,8 @@
 <?php
 
-use Silex\Application;
-use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\HttpFragmentServiceProvider;
-
-$app = new Application();
+$app = new Silex\Application(['debug' => true]);
 $app->register(new EucalyptusServiceProvider());
-
-$app['shunt'] = function (\Silex\Application $app) {
-
-    $result = $app['ec2.client']->describeInstances();
-    //$key = $input->getOption('private') ? 'private_dns_name' : 'public_dns_name';
-    $key = 'public_dns_name';
-
-    $hosts = [];
-    foreach ($result->search(EucalyptusServiceProvider::SEARCH_EXPRESSION) as $host) {
-        $hosts['ec2'][] = $host[$key];
-        if (isset($host['service'])) {
-            $hosts[$host['service']][] = $host[$key];
-        }
-    }
-
-
-    $recipe = array(
-        'hosts' => $hosts['bastion'],
-        'auth' => array(
-            'username' => 'debian',
-            'pubkeyfile' => '../ucsb-eucalyptus/bama.pub',
-            'privkeyfile' => '../ucsb-eucalyptus/baba.pem',
-            'passphrase' => NULL,
-        ),
-
-        'tasks' => array(
-            'ansible-galaxy' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-galaxy install -r requirements.yml');
-            },
-            'bind' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 100-bastion-bind.yml');
-            },
-            'couchdb' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 200-couchdb.yml');
-            },
-            'elasticsearch' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 200-elasticsearch.yml');
-            },
-            'worker-streaming' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 500-worker-streaming.yml');
-            },
-            'web-update' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 500-web-update.yml');
-            },
-            'kal3a-search' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 600-kal3a-search.yml');
-            },
-            'kal3a-tags' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 600-kal3a-tags.yml');
-            },
-            'all-update' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 500-all.yml');
-            },
-            'symfony' => function (\League\Shunt\Shunt $s) {
-                $s->run('cd cloud; source .env; source .env2; cd playbooks; ansible-playbook 900-reset-symfony.yml');
-            },
-            'vmstat' => function (\League\Shunt\Shunt $s) {
-                $s->run('vmstat -w');
-                $s->run('uptime');
-                $s->run('df');
-                $s->run('free -h');
-            },
-        ),
-    );
-
-    return new \League\Shunt\Console\Application($recipe, $app['debug']);
-};
+$app->register(new Silex\Provider\VarDumperServiceProvider());
 $app->register(new Aws\Silex\AwsServiceProvider(), [
     'aws.config' => function (Pimple\Container $c) {
         return [
@@ -85,5 +13,10 @@ $app->register(new Aws\Silex\AwsServiceProvider(), [
         ];
     },
 ]);
+
+$app['dotenv.loader'] = function () {
+    return new Dotenv\Dotenv(dirname(dirname(__DIR__)));
+};
+$app['dotenv.loader']->load();
 
 return $app;
